3dc552c2fc7cb8d4f2b8de628440b6b5e76e4304
Arthur Egide <arthuregide@gmail.com>
2020-05-01 22:54:11 -0300

    Inserção da configuração do banco de dados

--- src/main.js
+++ src/main.js
@@ -2,10 +2,31 @@ import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
+import store from './store'
+import firebase from 'firebase'
+import g from './../security'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
+const firebaseConfig = {
+  apiKey: g.APIKEY,
+  authDomain: "sancathon-socialfood.firebaseapp.com",
+  databaseURL: "https://sancathon-socialfood.firebaseio.com",
+  projectId: "sancathon-socialfood",
+  storageBucket: "sancathon-socialfood.appspot.com",
+  messagingSenderId: "990985202078",
+  appId: "1:990985202078:web:47cbbf8bc4f35a423bce8f"
+};
+firebase.initializeApp(firebaseConfig)
+let app= '';
+firebase.auth().onAuthStateChanged(()=>{
+  console.log("Auth changed")
+  if(!app){
+    console.log("!app")
+    app = new Vue({
+      router,
+      store,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
