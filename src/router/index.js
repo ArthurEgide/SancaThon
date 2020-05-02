b1fb01e6a518730c960b9aa96cb2da7fdf2f569c
Arthur Egide <arthuregide@gmail.com>
2020-05-01 22:55:25 -0300

    Rotas de login e registro criadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,54 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Register from '../views/Register.vue'
+import BookRegister from '../views/RegisterBook.vue'
+import NoteRegister from '../views/RegisterNote.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
+  {
+    path: "*",
+    redirect: "/login",
+  },
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: {
+      requeresAuth: true
+    }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'Login',
+    component: Login,
+    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register,
+    component: () => import(/* webpackChunkName: "about" */ '../views/Register.vue')
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const currentUser = firebase.auth().currentUser;
+  console.log("Navegando de: " , from)
+  console.log("Navegando para: " , to)
+  if (to.name !== 'Login' && !currentUser) next({ name: 'Login' })
+  if ((to.name == 'Login' || to.name == 'Register') && currentUser) next({ name: 'Home' })
+  else next()
 })
 
 export default router
